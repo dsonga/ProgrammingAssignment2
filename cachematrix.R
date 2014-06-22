dbd10b9a3b369ec33011e7df75bcab21cc4cacf6
dsonga <nicky_songa@yahoo.com>
2014-06-22 14:39:14 -0400

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cashable matrix objets in R
 
-## Write a short comment describing this function
+## the makecasmatrix function allows the user to make a matrix
+## using set command to nitialize the matrix
+## use a get command to retrieve the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+
+## Allows user to get the inverse of the marix created above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
